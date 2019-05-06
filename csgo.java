9bea0be1de548ad837d4c58446d000242f4f3ffc
nijiushi <547017470@qq.com>
2019-05-06 23:38:01 +0800

    提交代码

---
+++ csgo.java
@@ -0,0 +1,12 @@
+public class csgo{
+    public static void main(String[] args){
+		for(int i=1;i<10;i++)
+		{
+			for(int j=1;j<=i;j++)
+		{
+			System.out.print(i+"x"+j+"="+(j*i)+" ");
+		}
+		System.out.println();//换行
+	}
+	}
+}
